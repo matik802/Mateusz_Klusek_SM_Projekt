d3064b1dd9d20fa231589033bee86e537fe2a5c1
matik802 <mati88846@gmail.com>
2024-01-16 02:23:04 +0100

    app name change

--- app/src/main/java/com/example/book_rental/activities/LoginActivity.java
+++ app/src/main/java/com/example/book_rental/activities/LoginActivity.java
@@ -88,6 +88,8 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        getSupportActionBar().setTitle(R.string.login_activity);
     }
 
 }
